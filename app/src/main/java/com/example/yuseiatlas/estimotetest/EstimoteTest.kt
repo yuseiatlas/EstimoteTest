bb95349e7064f0b4ed063faa243ecd3b1b6db475
yuseiatlas <rasengan40@gmail.com>
2018-10-01 22:48:15 +0300

    Adds location permissions, RequirementsWizard & logging.

--- app/src/main/java/com/example/yuseiatlas/estimotetest/EstimoteTest.kt
+++ app/src/main/java/com/example/yuseiatlas/estimotetest/EstimoteTest.kt
@@ -1,9 +1,19 @@
 package com.example.yuseiatlas.estimotetest
 
 import android.app.Application
+import com.wafel.skald.api.LogLevel
+import com.wafel.skald.api.skald
+import com.wafel.skald.plugins.logcat.toLogcat
 
-class EstimoteTest: Application() {
+class EstimoteTest : Application() {
     override fun onCreate() {
         super.onCreate()
+        skald {
+            writeSaga {
+                toLogcat { withTag { "INDOOR-SDK" } }
+                withLevel { LogLevel.TRACE }
+                withPattern { "${it.simplePath} :: ${it.message}" }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/yuseiatlas/estimotetest/MainActivity.kt
+++ app/src/main/java/com/example/yuseiatlas/estimotetest/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.yuseiatlas.estimotetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.firebase.crash.FirebaseCrash
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +25,17 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, getString(R.string.location_id_hint), Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
-            startActivity(EstimoteActivity.getStartIntent(this, assetET.text.toString(), locationIdET.text.toString()))
+            launchEstimote()
         }
+    }
+
+    // checks locations permissions & Bluetooth status/permissions before launching EstimoteActivity
+    private fun launchEstimote() {
+        val requirementsWizard = RequirementsWizardFactory.createEstimoteRequirementsWizard()
+        requirementsWizard
+                .fulfillRequirements(this,
+                        { startActivity(EstimoteActivity.getStartIntent(this, assetET.text.toString(), locationIdET.text.toString())) },
+                        { Toast.makeText(this, "Unable to start BT advertising doe to missing requirements: $it", Toast.LENGTH_LONG).show() },
+                        { Toast.makeText(this, "Error occurred while obtaining BT permissions: $it", Toast.LENGTH_LONG).show() })
     }
 }
